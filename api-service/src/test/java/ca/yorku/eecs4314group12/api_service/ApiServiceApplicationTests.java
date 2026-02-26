a572aa0deda471ddc18644ab2d590b5017cd482a
richarc-yorku <richarc@my.yorku.ca>
2026-02-26 17:56:17 -0500

    api init
    created spring boot project with maven for the api service

---
+++ api-service/src/test/java/ca/yorku/eecs4314group12/api_service/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.yorku.eecs4314group12.api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
