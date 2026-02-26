878c576ac08b2a7eb2c64c7e63ae57f6a6939162
Rocco Fernando Gadista <157088428+r0cky-g@users.noreply.github.com>
2026-02-26 13:31:48 -0500

    Add Tmdb DTOs.

---
+++ movie-service/src/main/java/ca/yorku/eecs4314group12/movie/dto/TmdbCreditsActorDTO.java
@@ -0,0 +1,23 @@
+package ca.yorku.eecs4314group12.movie.dto;
+
+public class TmdbCreditsActorDTO {
+	
+	private String original_name;
+	private String character;
+	
+	public String getOriginal_name() {
+		return original_name;
+	}
+	
+	public void setOriginal_name(String original_name) {
+		this.original_name = original_name;
+	}
+	
+	public String getCharacter() {
+		return character;
+	}
+	
+	public void setCharacter(String character) {
+		this.character = character;
+	}
+}
---
+++ movie-service/src/main/java/ca/yorku/eecs4314group12/movie/dto/TmdbMovieDTO.java
@@ -0,0 +1,114 @@
+package ca.yorku.eecs4314group12.movie.dto;
+
+import java.util.ArrayList;
+
+public class TmdbMovieDTO {
+	
+	private int id;
+	private String original_language;
+	private String original_title;
+	private String overview;
+	private String poster_path;
+	private String release_date;
+	private int revenue;
+	private int runtime;
+	private String status;
+	private TmdbImagesDTO images;
+	private TmdbVideosDTO videos;
+	private TmdbCreditsDTO credits;
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getOriginal_language() {
+		return original_language;
+	}
+	
+	public void setOriginal_language(String original_language) {
+		this.original_language = original_language;
+	}
+	
+	public String getOriginal_title() {
+		return original_title;
+	}
+	
+	public void setOriginal_title(String original_title) {
+		this.original_title = original_title;
+	}
+	
+	public String getOverview() {
+		return overview;
+	}
+	
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+	
+	public String getPoster_path() {
+		return poster_path;
+	}
+	
+	public void setPoster_path(String poster_path) {
+		this.poster_path = poster_path;
+	}
+	
+	public String getRelease_date() {
+		return release_date;
+	}
+	
+	public void setRelease_date(String release_date) {
+		this.release_date = release_date;
+	}
+	
+	public int getRevenue() {
+		return revenue;
+	}
+	
+	public void setRevenue(int revenue) {
+		this.revenue = revenue;
+	}
+	
+	public int getRuntime() {
+		return runtime;
+	}
+	
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public TmdbImagesDTO getImages() {
+		return images;
+	}
+	
+	public void setImages(TmdbImagesDTO images) {
+		this.images = images;
+	}
+	
+	public TmdbVideosDTO getVideos() {
+		return videos;
+	}
+	
+	public void setVideos(TmdbVideosDTO videos) {
+		this.videos = videos;
+	}
+	
+	public TmdbCreditsDTO getCredits() {
+		return credits;
+	}
+	
+	public void setCredits(TmdbCreditsDTO credits) {
+		this.credits = credits;
+	}
+}
