e77cee22251ca277e0706d995ad8291038acca25
youwei <youwei@my.yorku.ca>
2026-02-26 19:05:35 -0500

    Add UserRepository, application.properties changes, mvnw updates

--- user-service/src/main/java/ca/yorku/eecs4314group12/user/repository/UserRepository.java
+++ user-service/src/main/java/ca/yorku/eecs4314group12/user/repository/UserRepository.java
@@ -5,5 +5,8 @@ import ca.yorku.eecs4314group12.user.model.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
