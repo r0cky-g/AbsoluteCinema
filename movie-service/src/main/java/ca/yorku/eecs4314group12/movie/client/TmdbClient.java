ff143dc9393ad77ea605d796cbbb5fcf2beaf516
Rocco Fernando Gadista <157088428+r0cky-g@users.noreply.github.com>
2026-02-26 21:41:29 -0500

    Decoupled TmdbClient from MovieService

---
+++ movie-service/src/main/java/ca/yorku/eecs4314group12/movie/client/TmdbClient.java
@@ -0,0 +1,28 @@
+package ca.yorku.eecs4314group12.movie.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import ca.yorku.eecs4314group12.movie.dto.TmdbMovieDTO;
+
+@Component
+public class TmdbClient {
+	
+	private final WebClient webClient;
+	private final String token;
+	
+	public TmdbClient(WebClient tdmbWebClient, @Value("${tmdb.token}") String tmdbToken) {
+		webClient = tdmbWebClient;
+		token = tmdbToken;
+	}
+	
+	public TmdbMovieDTO getMovieDetails(int id) {
+		return webClient.get()
+				.uri("/movie/"+id+"?append_to_response=images,videos,credits&language=en-US&include_image_langugage=null")
+				.header("accept", "application/json")
+				.header("Authorization", "Bearer "+token)
+				.retrieve()
+				.bodyToMono(TmdbMovieDTO.class)
+				.block();
+	}
+}
--- movie-service/src/main/java/ca/yorku/eecs4314group12/movie/service/MovieService.java
+++ movie-service/src/main/java/ca/yorku/eecs4314group12/movie/service/MovieService.java
@@ -1,34 +1,20 @@
 package ca.yorku.eecs4314group12.movie.service;
 
+import ca.yorku.eecs4314group12.movie.client.TmdbClient;
 import ca.yorku.eecs4314group12.movie.dto.TmdbMovieDTO;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Service
 public class MovieService {
 	
-	private String tmdbToken;
+	private final TmdbClient tmdbClient;
 	
-	public MovieService(@Value("${tmdb.token}") String tmdbToken) {
-		this.tmdbToken = tmdbToken;
+	public MovieService(TmdbClient tmdbClient) {
+		this.tmdbClient = tmdbClient;
 	}
 	
 	public TmdbMovieDTO getDetails(int id) {
-		return callTmdbApiDetails(id);
-	}
-	
-	private TmdbMovieDTO callTmdbApiDetails(int id) {
-		WebClient tmdbClient = WebClient.create("https://api.themoviedb.org/3");
-		
-		TmdbMovieDTO response = tmdbClient.get()
-										  .uri("/movie/"+id+"?append_to_response=images,videos,credits&language=en-US&include_image_langugage=null")
-										  .header("accept", "application/json")
-										  .header("Authorization", "Bearer "+tmdbToken)
-										  .retrieve()
-										  .bodyToMono(TmdbMovieDTO.class)
-										  .block();
-		
-		return response;
+		return tmdbClient.getMovieDetails(id);
 	}
 }
