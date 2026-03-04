5b9423822b0ba0e470897c4ff7645c3b7637b3e0
keppo <keppo10@my.yorku.ca>
2026-03-04 10:57:21 -0500

    Fixed Vaadin Styling and Updated .gitignore
    Fixed the styling issues from changing from vaadin 24 to 25 and added to the .gitignore to removed all the generated files when the project is built locally (22000+ files)

--- ui-service/src/main/frontend/generated/jar-resources/copilot.js
+++
@@ -1,5 +0,0 @@
-import { aA as a, az as i } from "./copilot/copilot-D0qmx7Vz.js";
-export {
-  a as createChildrenDefinitions,
-  i as registerImporter
-};
--- ui-service/src/main/java/ca/yorku/eecs4314group12/ui/UiServiceApplication.java
+++ ui-service/src/main/java/ca/yorku/eecs4314group12/ui/UiServiceApplication.java
@@ -3,8 +3,16 @@ package ca.yorku.eecs4314group12.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+/**
+ * @Theme wires Vaadin 25 to the Lumo design system and loads our custom
+ * styles from src/main/frontend/themes/absolute-cinema/styles.css
+ */
 @SpringBootApplication
-public class UiServiceApplication {
+@Theme("absolute-cinema")
+public class UiServiceApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(UiServiceApplication.class, args);
