8193c8f5afde3cfa20014f1a45f279aa6822c323
Rocco Fernando Gadista <157088428+r0cky-g@users.noreply.github.com>
2026-03-13 18:12:18 -0400

    Removed poster path for TmdbMovieDTO

--- movie-service/src/main/java/ca/yorku/eecs4314group12/movie/dto/TmdbMovieDTO.java
+++ movie-service/src/main/java/ca/yorku/eecs4314group12/movie/dto/TmdbMovieDTO.java
@@ -11,7 +11,6 @@ public class TmdbMovieDTO {
 	private String title;
 	private String tagline;
 	private String overview;
-	private String poster_path;
 	private String release_date;
 	private ArrayList<TmdbGenreDTO> genres;
 	private int budget;
@@ -76,14 +75,6 @@ public class TmdbMovieDTO {
 	
 	public void setOverview(String overview) {
 		this.overview = overview;
-	}
-	
-	public String getPoster_path() {
-		return poster_path;
-	}
-	
-	public void setPoster_path(String poster_path) {
-		this.poster_path = poster_path;
 	}
 	
 	public String getRelease_date() {
