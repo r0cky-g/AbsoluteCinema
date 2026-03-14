73526e7747b89b44abd4c52e7a00710f2116cf83
Rocco Fernando Gadista <157088428+r0cky-g@users.noreply.github.com>
2026-03-13 20:44:13 -0400

    Changed ArrayList to List for TmdbRegionDTO

--- movie-service/src/main/java/ca/yorku/eecs4314group12/movie/dto/TmdbRegionDTO.java
+++ movie-service/src/main/java/ca/yorku/eecs4314group12/movie/dto/TmdbRegionDTO.java
@@ -1,11 +1,11 @@
 package ca.yorku.eecs4314group12.movie.dto;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class TmdbRegionDTO {
 	
 	private String iso_3166_1;
-	private ArrayList<TmdbCertifcateDTO> release_dates;
+	private List<TmdbCertifcateDTO> release_dates;
 	
 	public String getIso_3166_1() {
 		return iso_3166_1;
@@ -15,11 +15,11 @@ public class TmdbRegionDTO {
 		this.iso_3166_1 = iso_3166_1;
 	}
 	
-	public ArrayList<TmdbCertifcateDTO> getRelease_dates() {
+	public List<TmdbCertifcateDTO> getRelease_dates() {
 		return release_dates;
 	}
 	
-	public void setRelease_dates(ArrayList<TmdbCertifcateDTO> release_dates) {
+	public void setRelease_dates(List<TmdbCertifcateDTO> release_dates) {
 		this.release_dates = release_dates;
 	}
 }
