09e33d0673202df8a0a21e4abb40225b8d58bf7a
Rocco Fernando Gadista <157088428+r0cky-g@users.noreply.github.com>
2026-03-13 23:21:49 -0400

    Implemented MovieMapper

---
+++ movie-service/src/main/java/ca/yorku/eecs4314group12/movie/mapper/MovieMapper.java
@@ -0,0 +1,59 @@
+package ca.yorku.eecs4314group12.movie.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import ca.yorku.eecs4314group12.movie.document.Movie;
+import ca.yorku.eecs4314group12.movie.dto.ActorDTO;
+import ca.yorku.eecs4314group12.movie.dto.CrewMemberDTO;
+import ca.yorku.eecs4314group12.movie.dto.MovieDTO;
+import ca.yorku.eecs4314group12.movie.dto.TmdbCertificateDTO;
+import ca.yorku.eecs4314group12.movie.dto.TmdbCompanyDTO;
+import ca.yorku.eecs4314group12.movie.dto.TmdbCreditsActorDTO;
+import ca.yorku.eecs4314group12.movie.dto.TmdbCreditsCrewDTO;
+import ca.yorku.eecs4314group12.movie.dto.TmdbGenreDTO;
+import ca.yorku.eecs4314group12.movie.dto.TmdbMovieDTO;
+import ca.yorku.eecs4314group12.movie.dto.TmdbReleaseDatesDTO;
+
+@Mapper(componentModel = "spring")
+public interface MovieMapper {
+	
+	@Mapping(source = "genres", target = "genres", qualifiedByName = "mapGenres")
+	@Mapping(source = "release_dates", target = "age_rating", qualifiedByName = "extractUSRating")
+	@Mapping(source = "credits.cast", target = "cast")
+	@Mapping(source = "credits.crew", target = "crew")
+	@Mapping(source = "production_companies", target = "production_companies", qualifiedByName = "mapProductionCompanies")
+	Movie toMovie(TmdbMovieDTO tmdbMovieDTO);
+	
+	MovieDTO toMovieDTO(Movie movie);
+	ActorDTO toActorDTO(TmdbCreditsActorDTO tmdbActorDTO);
+	CrewMemberDTO toCrewMemberDTO(TmdbCreditsCrewDTO tmdbCrewDTO);
+	
+	@Named("mapGenres")
+    default List<String> mapGenres(List<TmdbGenreDTO> genres) {
+        return genres.stream()
+                .map(TmdbGenreDTO::getName)  
+                .collect(Collectors.toList());
+    }
+	
+	@Named("extractUSRating")
+    default String extractUsRating(TmdbReleaseDatesDTO releaseDates) {
+        return releaseDates.getResults().stream()
+                .filter(r -> "US".equals(r.getIso_3166_1()))  
+                .findFirst()
+                .flatMap(r -> r.getRelease_dates().stream()
+                        .filter(d -> !d.getCertification().isEmpty())  
+                        .findFirst())
+                .map(TmdbCertificateDTO::getCertification)  
+                .orElse(null);
+    }
+	
+	@Named("mapProductionCompanies")
+    default List<String> mapProductionCompanies(List<TmdbCompanyDTO> companies) {
+        return companies.stream()
+                .map(TmdbCompanyDTO::getName)  
+                .collect(Collectors.toList());
+    }
+}
