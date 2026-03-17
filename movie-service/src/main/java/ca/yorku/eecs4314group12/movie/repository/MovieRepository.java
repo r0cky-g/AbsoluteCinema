4dfa9d3e0fa77946a2351b6d71c8ff055492a63a
Rocco Fernando Gadista <157088428+r0cky-g@users.noreply.github.com>
2026-03-16 21:56:21 -0400

    Refactored query in MovieRepository

--- movie-service/src/main/java/ca/yorku/eecs4314group12/movie/repository/MovieRepository.java
+++ movie-service/src/main/java/ca/yorku/eecs4314group12/movie/repository/MovieRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.Query;
 import ca.yorku.eecs4314group12.movie.document.Movie;
 
 public interface MovieRepository extends MongoRepository<Movie, Integer> {
-	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	@Query("{ title: { $regex: ?0, $options: 'i' } }")
     List<Movie> findByTitle(String keyword);
 }
