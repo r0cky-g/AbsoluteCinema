1f7744b1893bd70f8e6aac96d2ef6bcdfa174592
Rocco Fernando Gadista <157088428+r0cky-g@users.noreply.github.com>
2026-04-05 03:38:15 -0400

    Excluded the need of database when testing the in-memory caching

--- movie-service/src/test/java/ca/yorku/eecs4314group12/movie/service/MovieServiceCachingTests.java
+++ movie-service/src/test/java/ca/yorku/eecs4314group12/movie/service/MovieServiceCachingTests.java
@@ -8,39 +8,28 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.mongodb.MongoDBContainer;
-
 import ca.yorku.eecs4314group12.movie.client.TmdbClient;
 import ca.yorku.eecs4314group12.movie.dto.tmdb.TmdbMovieDTO;
 import ca.yorku.eecs4314group12.movie.dto.tmdb.TmdbMoviesNowPlayingDTO;
 import ca.yorku.eecs4314group12.movie.dto.tmdb.TmdbMoviesTrendingDTO;
+import ca.yorku.eecs4314group12.movie.repository.MovieRepository;
 
 @SpringBootTest
 @EnableCaching
-@Testcontainers
+@EnableAutoConfiguration(exclude = MongoAutoConfiguration.class)
 class MovieServiceCachingTests {
 	
-	@SuppressWarnings("resource")
-	@Container
-	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo")
-			.withEnv("MONGO_INITDB_ROOT_USERNAME", "root")
-	        .withEnv("MONGO_INITDB_ROOT_PASSWORD", "example")
-	        .withReuse(true);
+	@MockitoBean
+	private MovieRepository movieRepo;
 	
-	@DynamicPropertySource
-	static void setProperties(DynamicPropertyRegistry registry) {
-	    registry.add("spring.mongodb.uri", () ->
-	        "mongodb://root:example@" + mongoDBContainer.getHost() + ":" + mongoDBContainer.getFirstMappedPort()
-	    );
-	}
+	@MockitoBean
+	private TmdbClient tmdbClient;
 
     @Autowired
     private MovieService movieService;
@@ -48,9 +37,6 @@ class MovieServiceCachingTests {
     @Autowired
     CacheManager cacheManager;
     
-    @MockitoBean
-    private TmdbClient tmdbClient;
-
     @BeforeEach
     void setUp() {
         cacheManager.getCacheNames()
@@ -72,6 +58,7 @@ class MovieServiceCachingTests {
         
         TmdbMoviesTrendingDTO trending = new TmdbMoviesTrendingDTO();
         TmdbMoviesNowPlayingDTO nowPlaying = new TmdbMoviesNowPlayingDTO();
+        
         trending.setResults(List.of(movie1, movie2));
         nowPlaying.setResults(List.of(movie3, movie4));
         
