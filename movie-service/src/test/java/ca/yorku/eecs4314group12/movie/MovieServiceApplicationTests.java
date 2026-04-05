397c34e90b1c6eae782927817b1d1423a7482859
Rocco Fernando Gadista <157088428+r0cky-g@users.noreply.github.com>
2026-04-05 03:22:00 -0400

    ContextLoads now use mongo docker testcontainer

--- movie-service/src/test/java/ca/yorku/eecs4314group12/movie/MovieServiceApplicationTests.java
+++ movie-service/src/test/java/ca/yorku/eecs4314group12/movie/MovieServiceApplicationTests.java
@@ -2,9 +2,29 @@ package ca.yorku.eecs4314group12.movie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mongodb.MongoDBContainer;
 
 @SpringBootTest
+@Testcontainers
 class MovieServiceApplicationTests {
+	
+	@SuppressWarnings("resource")
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo")
+			.withEnv("MONGO_INITDB_ROOT_USERNAME", "root")
+	        .withEnv("MONGO_INITDB_ROOT_PASSWORD", "example")
+	        .withReuse(true);
+	
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+	    registry.add("spring.mongodb.uri", () ->
+	        "mongodb://root:example@" + mongoDBContainer.getHost() + ":" + mongoDBContainer.getFirstMappedPort()
+	    );
+	}
 
 	@Test
 	void contextLoads() {
